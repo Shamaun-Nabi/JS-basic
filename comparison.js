967383c54f5d411867dd0a6a6a999356e351f3ca
Shamaun-Nabi <snashik950@gmail.com>
2023-01-07 22:14:45 +0600

    condition logical operator,comparison

---
+++ comparison.js
@@ -0,0 +1,33 @@
+// > is the greater than operator
+// console.log(5 > 6);
+
+// < is the less than operator
+//  console.log(5 < 6);
+
+// <= is the less than or equal to operator
+// console.log(6<=6);
+
+// >= is the greater than or equal to operator
+// console.log(7>=6);
+
+// == is the equal to operator
+// console.log(85 == 85);
+
+// != is the not equal to operator
+// console.log(80 != 80);
+
+// let apnarValobasha = 50;
+// let tarValobasha = 40;
+
+// console.log(apnarValobasha > tarValobasha);
+// console.log(apnarValobasha < tarValobasha);
+
+// let gotJob;
+// let moneySaved;
+
+//  "&& "is the "and" operator
+// console.log(gotJob && moneySaved);
+// || is the "or" operator
+// console.log(gotJob || moneySaved);
+
+
---
+++ condition.js
@@ -0,0 +1,30 @@
+// let teaIsReady = true;
+
+// if (teaIsReady == true) {
+//   console.log("Bichana theke uthbo");
+// }
+// else{
+//     console.log("Bichana theke uthbo na");
+// }
+
+// let iphonePrice = 70000;
+// let myMoney = 9000;
+
+// console.log(iphonePrice < myMoney);
+
+// if (iphonePrice < myMoney) {
+//   console.log("Iphone diye futani marbo");
+// }
+// else{
+//     console.log("Kinte parbo na vab o dekhate parbo na")
+// }
+
+let chickenPrice = 180;
+let myMoney = 190;
+
+if( chickenPrice>myMoney){
+    console.log("Khamu");
+}
+else{
+    console.log("Khamu na");
+}
