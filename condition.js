d7fd833e29cc57b12583e17d595fc7503719ec0d
Shamaun-Nabi <snashik950@gmail.com>
2023-01-09 22:13:03 +0600

    conditions example

--- condition.js
+++ condition.js
@@ -19,12 +19,62 @@
 //     console.log("Kinte parbo na vab o dekhate parbo na")
 // }
 
-let chickenPrice = 180;
-let myMoney = 190;
+// let chickenPrice = 180;
+// let myMoney = 190;
 
-if( chickenPrice>myMoney){
-    console.log("Khamu");
-}
-else{
-    console.log("Khamu na");
-}
+// if( chickenPrice>myMoney){
+//     console.log("Khamu");
+// }
+// else{
+//     console.log("Khamu na");
+// }
+
+// -2-1 0 1 2 3 4 5
+
+// Find Positive & Negative Number
+// const userInput = -5;
+
+// if (userInput > 0) {
+//   console.log(userInput, " " + "is a Positive Number");
+// }
+// else if (userInput == 0) {
+//   console.log(userInput, " neither positive nor negative");
+// } else {
+//   console.log("Negative");
+// }
+
+// even = jor songkha
+//  odd = bijor songkha
+
+// let userInput = 4; //
+
+// console.log("Tumi ki disso?",userInput % 2);
+
+// if (userInput % 2 == 0) {
+//   console.log("Even ba Jor Sonkgha");
+// } else {
+//   console.log("Bijor odd songkha");
+// }
+
+// const firstNumber = 500;
+// const secondNumber = 100;
+
+// if (firstNumber > secondNumber) {
+//   console.log(firstNumber, "is Greater Number");
+// }
+// else{
+//     console.log(secondNumber, "is Greater Number");
+// }
+
+// const firstNumber = 25;
+// const secondNumber = 10;
+// const thirdNumber = 50;
+
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//   console.log(firstNumber, "is Greater Number");
+// } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+//   console.log(secondNumber, "is Greater Number");
+// }
+// else{
+//     console.log(thirdNumber, "is Greater Number");
+// } 
