6b8b6c38d728b66337b775b8e23862de9c98c3aa
Shamaun-Nabi <snashik950@gmail.com>
2023-01-12 21:01:20 +0600

    for loop

--- loop.js
+++ loop.js
@@ -38,12 +38,64 @@
 //   //   num++;
 // }
 
-let startPoint = 0;
-let sum = 0;
-while (startPoint <= 10) {
-  console.log("code running", startPoint);
-  sum = sum + startPoint;
-  console.log(sum);
-  startPoint = startPoint + 1;
+// let startPoint = 0;
+// let sum = 0;
+// while (startPoint <= 10) {
+//   console.log("code running", startPoint);
+//   sum = sum + startPoint;
+//   console.log(sum);
+//   startPoint = startPoint + 1;
+// }
+// console.log("This is result of sum", sum);
+
+// let myStores = [14, 17, 15, 17, 69, 75, 98,100,17,15];
+
+// console.log(myStores.length);
+// console.log(myStores[3]);
+
+// if (condition) {
+//   // run korbe;
+// }
+
+// loop
+// let startPoint = 0;
+// while (condition) {
+//   // run korbe;
+//   // startPoint++;
+//   // startPoint=startPoint+1;
+//   // startPoint += 1;
+// }
+
+// let startPoint = 1;
+// while (startPoint <= 10) {
+//   console.log(startPoint);
+//   startPoint = startPoint + 1;
+// }
+
+// for (
+//   let startPointForLOOP = 1;
+//   startPointForLOOP <= 10;
+//   startPointForLOOP = startPointForLOOP + 1
+// ) {
+//   console.log(startPointForLOOP);
+// }
+
+// let startPoint = 1;
+// while (startPoint <= 4) {
+//   console.log(startPoint);
+//   startPoint = startPoint + 1;
+// }
+
+// for (let start = 1; start <= 4; start = start + 1) {
+//   console.log(start);
+// }
+
+let myFriendAges = [
+  14, 17, 22, 23, 26, 78, 45, 47, 89, 45, 78, 75, 56, 78, 547, 5647
+];
+
+// console.log(myFriendAges);
+
+for (let iteration = 2; iteration < myFriendAges.length * 2; iteration + 2) {
+  console.log(myFriendAges[iteration]);
 }
-console.log("This is result of sum", sum);
